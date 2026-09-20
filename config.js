38526c11ba3c84153559b6702a484e2a115f29df
sirmotors4-questionarios <sirmotors4@gmail.com>
2026-09-20 20:12:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CHECKLIST_CONFIG = {
   masterDataUrl: 'https://sirmotors4-questionarios.github.io/diario-bordo/data/master-data.json',
   submissionUrl: 'https://default5fc7bb0c4c3f4008ae039efd62fc96.c9.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/13/workflows/49a3729e61594a45973a2514603c6de8/triggers/manual/paths/invoke?api-version=1',
-  demoMode: true
+  demoMode: false
 };
